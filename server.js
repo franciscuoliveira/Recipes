e14ce403bdeb40b33440e1d23d41c06d713d5b07
franciscuoliveira <58727899+franciscuoliveira@users.noreply.github.com>
2022-05-29 18:54:41 +0100

    try

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(bodyParser.json())
 app.use(logger('dev'))
 app.use(errorhandler())
 
-const PORT = process.eventNames.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
 //Imports Routes
 const postsRoute = require('./routes/posts');
